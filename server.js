50e7547dfab0f01a19e6792b515f33847ca2b287
Thinh Nguyen <thinh-nguyen@hotmail.com>
2020-06-06 18:52:27 -0400

    ex3.4 sign in page receives input and redirects

--- server.js
+++ server.js
@@ -27,8 +27,33 @@ const handleProfilePage = (req, res) => {
   // res.status(200).send(req.params.id);
 };
 
+//render the sign in page
 const handleSignin = (req, res) => {
   res.status(200).render("pages/signin");
+};
+
+//redirect signin page to user, when logged in
+const handleName = (req, res) => {
+  //the req.query returns what was inputed in the form of the sign in page
+  //as GET /getname?firstName=Russel, req.query accesses the key firstname via
+  //dot method
+  let firstName = req.query.firstName;
+  //console.log(firstName); //to help confirm the above
+
+  //use the find method on the users data to return the object with the same
+  //name entered, this will be our "login" method
+  let loginName = users.find((user) => user.name === firstName);
+  console.log(loginName); //check if it returns the object
+
+  //if the user inputs a valid name that exists in our data file then it will
+  //sucessfully redirect user to that profile page
+  if (loginName != undefined) {
+    res.status(200).redirect(`/users/${loginName._id}`);
+  } else {
+    //if the input doesnt match anything on our data file, then it will re-render
+    //the page
+    res.status(404).render("pages/signin");
+  }
 };
 
 // -----------------------------------------------------
@@ -49,6 +74,9 @@ express()
 
   //go to sign in page
   .get("/signin", handleSignin)
+
+  //
+  .get("/getname", handleName)
 
   // a catchall endpoint that will send the 404 message.
   .get("*", handleFourOhFour)
